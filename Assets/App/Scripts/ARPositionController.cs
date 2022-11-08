53179be0d9beb4f6eeabd0ebfba0d69b6b4ffbb7
MedCam <61634768+MedCam@users.noreply.github.com>
2022-11-08 17:52:06 +0100

    Project

---
+++ Assets/App/Scripts/ARPositionController.cs
@@ -0,0 +1,22 @@
+﻿using MRTKExtensions.QRCodes;
+using UnityEngine;
+
+public class ARPositionController : MonoBehaviour
+{
+    [SerializeField]
+    private QRTrackerController trackerController;
+    [SerializeField] private AnchorModuleScript anchorModule;
+    private void Start()
+    {
+        trackerController.PositionSet += PoseFound;
+    }
+
+    private void PoseFound(object sender, Pose pose)
+    {
+        var childObj = transform.GetChild(0);
+        childObj.SetPositionAndRotation(pose.position, pose.rotation);
+        childObj.gameObject.SetActive(true);
+        //To create anchor in QR Code Position;
+        //anchorModule.CreateAzureAnchor(childObj.gameObject);
+    }
+}
